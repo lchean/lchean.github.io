902df0c917c318736aa1a8c373bd3fa86e11b19f
Laurent Chean <laurentchean@Laurents-MacBook-Pro.local>
2018-12-09 18:15:50 +0100

    Updates

--- precache-manifest.1fd6bce7533372beb6fba939b6ca15f6.js
+++ precache-manifest.1fd6bce7533372beb6fba939b6ca15f6.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "2304b48719c3312048f3",
-    "url": "/static/js/main.2304b487.chunk.js"
+    "revision": "9cfefcc51d42be542c07",
+    "url": "/static/js/main.9cfefcc5.chunk.js"
   },
   {
     "revision": "9d212a5dcd6b8d88d213",
     "url": "/static/js/1.9d212a5d.chunk.js"
   },
   {
-    "revision": "2304b48719c3312048f3",
-    "url": "/static/css/main.cdd4da04.chunk.css"
+    "revision": "9cfefcc51d42be542c07",
+    "url": "/static/css/main.8f86a316.chunk.css"
   },
   {
     "revision": "9d212a5dcd6b8d88d213",
     "url": "/static/css/1.89937640.chunk.css"
   },
   {
-    "revision": "47ca7bed78313eca62747b114ba33b70",
+    "revision": "9ae26270c0f94aedb8922fa188e3540d",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1d7d4cfdccae910b8aeee56b1b7640fc.js"
+  "/precache-manifest.1fd6bce7533372beb6fba939b6ca15f6.js"
 );
 
 workbox.clientsClaim();

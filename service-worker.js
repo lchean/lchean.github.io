73d3e45ac8c2eefdbc339dbd426a14cbf73dd960
Laurent Chean <laurentchean@gmail.com>
2018-12-28 18:41:19 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7c3a8a343220df76f8123dee9d4e1b54.js"
+  "/precache-manifest.d37ab666caeab4d50227c24639f43895.js"
 );
 
 workbox.clientsClaim();

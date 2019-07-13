f3e85f6e3415924c90805306af596fd2991653e2
Laurent Chean <laurentchean@gmail.com>
2019-07-13 15:00:16 +0200

    update text

--- src/Pages/About/index.js
+++ src/Pages/About/index.js
@@ -6,29 +6,42 @@ const opts = {
     height: '720',
     width: '1280',
     playerVars: {
-        autoplay: 1
-    }
+        autoplay: 1,
+    },
 };
 
 export default class About extends React.Component {
-    render() {          
+    render() {
         return (
             <article>
-                <h2>More</h2>     
-                <section className="about-section">               
+                <h2>More</h2>
+                <section className='about-section'>
                     <ul>
                         <li>Advanced skills in HTML, CSS, and JS</li>
-                        <li>Good understanding of Wordpress, Magento 1, Salesforce Commerce Cloud (Demandware)</li>
-                        <li>This website uses the <a href="https://fonts.google.com/specimen/EB+Garamond">EB Garamond Google font</a> and was built with <a href="https://github.com/facebook/create-react-app">Create React App</a> and <a href="https://pages.github.com/">Github Pages</a></li>
-                        <li className="no-mobile">The photos are from <a href="https://www.hedislimane.com/diary/">Hedi Slimane Diary</a></li>
+                        <li>
+                            Good understanding of Wordpress, Magento 1,
+                            Salesforce Commerce Cloud (Demandware)
+                        </li>
+                        <li>
+                            This website uses the{' '}
+                            <a href='https://fonts.google.com/specimen/EB+Garamond'>
+                                EB Garamond Google font
+                            </a>{' '}
+                            and was built with{' '}
+                            <a href='https://github.com/facebook/create-react-app'>
+                                Create React App
+                            </a>{' '}
+                            and{' '}
+                            <a href='https://pages.github.com/'>Github Pages</a>
+                        </li>
                     </ul>
                 </section>
 
-                <section className="about-section youtube-video">
-                    <p>Life is too serious so let's listen to some good french (summer) music :</p>
-                    <figure className="youtube">
+                <section className='about-section youtube-video'>
+                    <p>Let's listen to some good french music :</p>
+                    <figure className='youtube'>
                         <YouTube
-                            videoId="p4JO2w5uGfE"
+                            videoId='p4JO2w5uGfE'
                             opts={opts}
                             onReady={this._onReady}
                         />
@@ -38,5 +51,3 @@ export default class About extends React.Component {
         );
     }
 }
-
-

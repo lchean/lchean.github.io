e6688a9d3c8c1859f6cf8e82e587907c581e3581
Laurent Chean <laurentchean@gmail.com>
2020-03-03 22:25:16 +0100

    Updates

---
+++ precache-manifest.453facc148bfbcd734beedd8597c1930.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "826e8cde9c3660bb8725285fa2032845",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e3a45a6a941945252a2",
+    "url": "/static/css/2.a3664874.chunk.css"
+  },
+  {
+    "revision": "e707cc5eab82d7e97eff",
+    "url": "/static/css/main.b0b48e6b.chunk.css"
+  },
+  {
+    "revision": "2e3a45a6a941945252a2",
+    "url": "/static/js/2.9a6a8ab6.chunk.js"
+  },
+  {
+    "revision": "63ba49e0547f655e17d784069efb1191",
+    "url": "/static/js/2.9a6a8ab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e707cc5eab82d7e97eff",
+    "url": "/static/js/main.d13ee411.chunk.js"
+  },
+  {
+    "revision": "affe0b6a18ee5e21b8a8",
+    "url": "/static/js/runtime-main.7101afcb.js"
+  }
+]);
